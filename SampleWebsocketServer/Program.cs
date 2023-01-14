6a041aa337ffe9dffe1f2dbf66f4468ea835de16
Jeremy Wong <navhaxs@hotmail.com>
2023-01-14 12:30:26 +1100

    first commit

---
+++ SampleWebsocketServer/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SampleWebsocketServer
+{
+    using System;
+    using EmbedIO;
+    using EmbedIO.Actions;
+    using EmbedIO.WebApi;
+    using Swan.Logging;
+
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8979/";
+            if (args.Length > 0)
+                url = args[0];
+
+            // Our web server is disposable.
+            using (var server = CreateWebServer(url))
+            {
+                // Once we've registered our modules and configured them, we call the RunAsync() method.
+                server.RunAsync();
+
+                // Wait for any key to be pressed before disposing of our web server.
+                // In a service, we'd manage the lifecycle of our web server using
+                // something like a BackgroundWorker or a ManualResetEvent.
+                Console.ReadKey(true);
+            }
+        }
+
+        // Create and configure our web server.
+        private static WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                    .WithModule(new WebSocketChatServer("/"))
+            ;
+
+            // Listen for state changes.
+            server.StateChanged += (s, e) => $"WebServer New State - {e.NewState}".Info();
+
+            return server;
+        }
+    }
+}
+
---
+++ SampleWebsocketServer/WebSocketChatServer.cs
@@ -0,0 +1,26 @@
+﻿namespace SampleWebsocketServer
+{
+    using EmbedIO.WebSockets;
+
+    /// <summary>
+    /// Defines a very simple chat server.
+    /// </summary>
+    public class WebSocketChatServer : WebSocketModule
+    {
+        public WebSocketChatServer(string urlPath)
+            : base(urlPath, true)
+        {
+            // placeholder
+        }
+
+        /// <inheritdoc />
+        protected override Task OnMessageReceivedAsync(
+            IWebSocketContext context,
+            byte[] rxBuffer,
+            IWebSocketReceiveResult rxResult)
+        {
+            Console.WriteLine($"RECEIVED DATA OK - {Encoding.GetString(rxBuffer)}");
+            return Task.CompletedTask;
+        }
+    }
+}
